92749834be8baf488287de1d124adc30a03a08c3
Koketso <koketsogaowelwe2@gmail.com>
2025-10-14 07:30:21 +0000

    IzzytheIguana

--- IzzytheIguana.java
+++ IzzytheIguana.java
@@ -15,12 +15,12 @@ public class IzzytheIguana{
         String userInput = input.nextLine();
         for (int i = 0; i < userInput.length(); i++){
             String result = "";
-            if (userInput[i].equals(" ")){
+            if (userInput.valueOf(i).equals(" ")){
                 snacks.add(result);
                 continue;
             }
             else{
-                result += userInput[i];
+                result += userInput.valueOf(i);
             }
         }
 
@@ -36,7 +36,7 @@ public class IzzytheIguana{
                 points += mango;
             }
             else{
-                point += cheeseBurger;
+                points += cheeseBurger;
             }
         }
 
