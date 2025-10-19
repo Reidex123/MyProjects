87f4981994830519a9ed801253a5750c7dfda7c1
Koketso <koketsogaowelwe2@gmail.com>
2025-10-19 16:11:57 +0000

    numberOfOnes.java

---
+++ numberOfOnes.java
@@ -0,0 +1,17 @@
+public class numberOfOnes{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int integer = input.nextInteger();
+        String binary = Integer.toBinaryString(integer);
+
+        int countOnes = 0;
+        for (int i = 0; i < binary.length(); i++){
+            if (binary.charAt(i) == '1'){
+                countOnes++;
+            }
+        }
+
+        System.out.println(countOnes);
+    }
+}
