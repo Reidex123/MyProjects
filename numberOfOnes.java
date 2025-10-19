14263b8e21bd425acd271e12a2d4f2d3e4952f69
Koketso <koketsogaowelwe2@gmail.com>
2025-10-19 18:20:39 +0200

    Fix input method from nextInteger to nextInt

--- numberOfOnes.java
+++ numberOfOnes.java
@@ -4,7 +4,7 @@ public class numberOfOnes{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        int integer = input.nextInteger();
+        int integer = input.nextInt();
         String binary = Integer.toBinaryString(integer);
 
         int countOnes = 0;
