4f093629ea63606b4479fe5eabc5fac6f3abb9be
Koketso <koketsogaowelwe2@gmail.com>
2025-10-20 13:01:00 +0200

    Refactor total method to use order instead of customer

--- Kaleidoscopes.java
+++ Kaleidoscopes.java
@@ -1,18 +1,19 @@
 import java.util.*;
 
 public class Kaleidoscopes{
+  
   public static double discount = 0.1d;
   public static double tax = 0.07d;
   public static double productCost = 5.0d;
 
-  public static double total(int customer){
+  public static double total(int order){
     double total = 0.0d;
-    double quantityCost = customer*productCost;
+    double quantityCost = order*productCost;
 
     double discountAmount = discount*quantityCost;
     double taxAmount = tax*quantityCost;
     
-    if (customer > 1){
+    if (order > 1){
       // calculate the total with discount
       total = quantityCost - discountAmount + taxAmount;      
     } else{
@@ -22,12 +23,14 @@ public class Kaleidoscopes{
 
     return total;
   }
+
+  // Main program
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
 
-    int customer = input.nextInt();
+    int order = input.nextInt();
 
-    double cost = total(customer);
+    double cost = total(order);
 
     System.out.println(cost);
   }
