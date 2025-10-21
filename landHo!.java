189f8bd2fe9c1d0421444b4feb08cb415c913fa0
Koketso <koketsogaowelwe2@gmail.com>
2025-10-21 09:55:46 +0200

    Update waiting time calculation in landHo! class
    Refactor waiting time calculation to use numPassengers.

--- landHo!.java
+++ landHo!.java
@@ -1,9 +1,19 @@
 import java.util.*;
 
 public class landHo!{
+  public static int trip = 10;
+
+  // determine waiting time
+  public static int total(int numPassengers){
+    return (!(numPassengers/20 == 0) ? (numPassengers/20)*trip: trip); 
+  }
+  
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
     
-    int passengersAhead = input.nextInt();
+    int numPassengers = input.nextInt();
+    int time = total(numPassengers);
+
+    System.out.println(time);
   }
 }
