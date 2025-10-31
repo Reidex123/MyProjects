30314aaf261d4134805c56df5cd04585f7f08a80
Koketso <koketsogaowelwe2@gmail.com>
2025-10-31 17:59:09 +0200

    NeverLand

--- neverLand.java
+++ neverLand.java
@@ -3,5 +3,15 @@ import java.util.*;
 public class neverLand{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
+
+    int myAge = input.nextInt();
+    int yearsElapsed = input.nextInt();
+
+    int twinAge = myAge + yearsElapsed;
+
+    if (yearsElapsed <= 1)
+      System.out.printf("My twin is %d years old and they are %d year older than me", twinAge, yearsElapsed);
+    else
+      System.out.printf("My twin is %d years old and they are %d years older then me", twinAge, yearsElapsed);
   }
 }
