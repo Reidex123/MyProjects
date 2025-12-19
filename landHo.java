874c72f2191091a680d75140effcf9a3484d4d41
Koketso <koketsogaowelwe2@gmail.com>
2025-12-19 21:19:30 +0000

    Fix formatting issues in total method and main method

--- landHo.java
+++ landHo.java
@@ -5,15 +5,17 @@ public class landHo{
 
   // determine waiting time
   public static int total(int numPassengers){
-    return (!(numPassengers/20 == 0) ? numPassengers + trip: trip; 
+    return !(numPassengers/20 == 0) ? numPassengers + trip: trip;
   }
-  
+
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
-    
+
     int numPassengers = input.nextInt();
     int time = total(numPassengers);
 
     System.out.println(time);
+
+    input.close();
   }
 }
