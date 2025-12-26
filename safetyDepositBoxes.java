50e0e269cbeb1f6cc55801373ef96733430bb195
Koketso <koketsogaowelwe2@gmail.com>
2025-12-26 15:44:44 +0000

    Add safety deposit boxes functionality and update README

---
+++ safetyDepositBoxes.java
@@ -0,0 +1,18 @@
+public class safetyDepositBoxes{
+    public static void main(String[] args){
+        java.util.Scanner input = new java.util.Scanner(System.in);
+
+        String[] box = (input.nextLine()).split(",");
+        String target = input.nextLine();
+
+        int time = 0;
+        for (String item: box){
+
+            if(item.equalsIgnoreCase(target))
+                time += 5;
+            else
+                continue;
+                
+        }
+    }
+}
