04abe704a9bba8aaab6b203cf3ae67505cf4c31a
Koketso <koketsogaowelwe2@gmail.com>
2026-02-28 21:35:21 +0200

    implemented initials.java, final

--- initials.java
+++ initials.java
@@ -1,5 +1,41 @@
 import java.util.*;
 
-public class initials{
+public class initials {
+    public static final List<String[]> names = new ArrayList<>();
+
+    public static void evaluateInitials(List<String[]> names) {
+
+        for (String[] name : names) {
+            System.out.print(process(name) + " ");
+        }
+
+    }
+
+    private static String process(String[] name) {
+        String tempInit = "";
+
+        for (String word : name) {
+            tempInit += word.charAt(0);
+        }
+
+        return tempInit;
+    }
+
+    public static void main(String[] args) {
+
+        try (Scanner input = new Scanner(System.in)) {
+            int query = input.nextInt();
+            input.nextLine();
+
+            for (int i = 0; i < query; i++) {
+                String nameSurname = input.nextLine();
+                names.add(nameSurname.split(" "));
+            }
+
+            evaluateInitials(names);
+
+        }
+
+    }
 
 }
