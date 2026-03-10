367569eb820169c1d9ec79a957c58ab12e44654f
Koketso <koketsogaowelwe2@gmail.com>
2026-03-10 16:42:25 +0000

    finished flowingWords

--- flowingWords.java
+++ flowingWords.java
@@ -1,5 +1,6 @@
 public class flowingWords{
 
+    public static final java.util.Stack<Character> lettersStack = new java.util.Stack<>();
     public static void main(String[] args){
 
         try(java.util.Scanner input = new java.util.Scanner(System.in)){
@@ -7,7 +8,32 @@ public class flowingWords{
             System.out.println("Enter input string(sentence):");
             String[] sentenceWords = (input.nextLine()).split(" ");
 
-            System.out.println(sentenceWords);
+            boolean results = false;
+
+            for (String word: sentenceWords){
+                if (lettersStack.empty()){
+                    lettersStack.push(java.lang.Character.valueOf(word.charAt(0)));
+                    lettersStack.push(java.lang.Character.valueOf(word.charAt(word.length()-1)));
+                }
+
+                else{
+
+                    if (java.lang.Character.toString(lettersStack.peek()).equals(java.lang.Character.toString(Character.valueOf(word.charAt(0))))){
+                        lettersStack.push(java.lang.Character.valueOf(word.charAt(word.length()-1)));
+                        results = true;
+                    }else{
+                        results = false;
+                        break;
+                    }
+                }
+            }
+
+            if (results){
+                System.out.println(results);
+                Runtime.getRuntime().exit(0);
+            }
+
+            System.out.println(results);
         }
 
     }
